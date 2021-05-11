1ca467c1d7cb32233a82c9297ba9a7248f5179e5
Alex <chel213@ya.ru>
2021-05-11 15:42:31 +0300

    feat: decorates cli

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require(`fs`);
+const chalk = require(`chalk`);
 const {getRandomInt, shuffle} = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
@@ -84,18 +85,18 @@ module.exports = {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     if (countOffer > MAX_COUNT) {
-      console.info(`Не больше ${MAX_COUNT} объявлений`);
+      console.info(chalk.red(`Не больше ${MAX_COUNT} объявлений`));
       process.exit(ExitCode.error);
     }
     const content = JSON.stringify(generateOffers(countOffer));
 
     fs.writeFile(FILE_NAME, content, (err) => {
       if (err) {
-        console.error(`Can't write data to file...`);
+        console.error(chalk.red(`Can't write data to file...`));
         process.exit(ExitCode.error);
       }
 
-      console.info(`Operation success. File created.`);
+      console.info(chalk.green(`Operation success. File created.`));
       process.exit(ExitCode.success);
     });
   }
--- src/service/cli/version.js
+++ src/service/cli/version.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const chalk = require(`chalk`);
 const jsonFile = require(`../../../package.json`);
 
 module.exports = {
   name: `--version`,
   run() {
     const version = jsonFile.version;
-    console.info(version);
+    console.info(chalk.blue(version));
   }
 };
 
